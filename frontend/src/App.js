76d6e366405b9e35e7b46a02c66d32379a177f15
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-23 19:16:26 +0900

    20 User in Redux State

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,28 @@ import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
 import { auth } from "./firebase";
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-  
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+
+    const unsubscribe = auth.onAuthStateChanged( async (user) =>{
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult()
+        console.log('user', user)
+        dispatch({
+          type: 'USER_LOGGED_IN',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    })
+    return () => unsubscribe()
+  },[])
 
   return (
     <>
