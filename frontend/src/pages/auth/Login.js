5547243fe56bcfd3e963a52da91e2427ea03ca51
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-28 17:16:10 +0900

    22 Login Page

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
+import { Button } from 'antd'
+import {
+  MailOutlined
+} from "@ant-design/icons";  
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  const loginForm = () => (
+    <form onSubmit={submitHandler}>
+      <div className='form-group'>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='Your Email'
+        autoFocus
+      />
+      </div>
+      <div className='form-group'>
+      <input
+        type="password"
+        className="form-control"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='Your Password'
+      /> 
+      </div>
+       
+      <br />
+      <Button
+        onClick={submitHandler}
+        type='primary'
+        className='mb-3'
+        block
+        shape='round'
+        icon={<MailOutlined />}
+        size='large'
+        disabled={!email || password.length < 6}
+      >Login with Email/Password</Button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Login</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {loginForm()}
+        </div>
+      </div>
     </div>
   );
 };
