187a6717862a3d60f88a6787cd48028985dc3731
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-29 23:35:14 +0900

    Password Reset Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from "react"
+import { Switch, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 //Components
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
-import Header from "./components/nav/Header";
-import RegisterComplete from "./pages/auth/RegisterComplete";
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import Home from "./pages/Home"
+import Header from "./components/nav/Header"
+import RegisterComplete from "./pages/auth/RegisterComplete"
+import ForgotPassword from './pages/auth/ForgotPassword'
 
-import { auth } from "./firebase";
+import { auth } from "./firebase"
 import { useDispatch } from 'react-redux'
 
 const App = () => {
@@ -43,6 +44,7 @@ const App = () => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/register/complete" component={RegisterComplete} />
+        <Route exact path='/forgot/password' component={ForgotPassword} />
       </Switch>
     </>
   );
