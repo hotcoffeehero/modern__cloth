81b4308bb59911ba4b740dfc252b0fc49aa08e25
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-04 01:18:38 +0900

    Create/Update User in DB

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,22 @@
+const { findOneAndUpdate } = require('../models/user')
+const User = require('../models/user')
 
+exports.createOrUpdateUser = async (req, res)=>{
+    const { name, picture, email } = req.user
 
-exports.createOrUpdateUser = (req, res)=>{
-    res.json({
-        data: 'Create or update user api endpoint'
-    })
+    //Check if a user exists or not
+    const user = await User.findOneAndUpdate({email}, {name, picture}, {new:true})
+
+    if(user){
+        res.json(user)
+        console.log('USER:', user)
+    } else {
+        const newUser = await new User({
+            email,
+            name,
+            picture,
+        }).save()
+        console.log("NEW USER:", newUser)
+        res.json(newUser)
+    }
 }
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,19 @@
 const admin = require('../firebase')
 
-exports.authCheck = (req,res,next) => {
-    console.log(req.headers)
-    next()
+exports.authCheck = async (req,res,next) => {
+    try {
+        const fbUser = await admin
+        .auth()
+        .verifyIdToken(req.headers.authtoken)
+        // console.log("FirebaseUser: ", fbUser)
+        
+        req.user = fbUser
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(401)
+        .json({
+            error: 'Invalid or Expired Token'
+        })
+    }
 }
